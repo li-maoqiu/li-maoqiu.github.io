196c1d4d90cd8613bfdb51a29435b44f1afe6754
LiMaoQiu <123719932@qq.com>
2025-04-21 23:32:23 +0800

    。

---
+++ biji/博客/像素中国地图.js
@@ -0,0 +1,71 @@
+let zgdt=[
+    "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,黑龙江 aoaa,黑龙江 aooo,黑龙江 aaoo,0,0,0,黑龙江 aaoa",
+    "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,内蒙古 aooa,内蒙古 aaoo,黑龙江 ooaa,黑龙江 oooo,黑龙江 aooo,黑龙江 aooo,黑龙江 aooo,黑龙江 oaao",
+    "0,0,0,0,0,0,0,0,0,0,0,0,0,0,内蒙古 aoaa,内蒙古 oooo,内蒙古 oooo,内蒙古 aaao,黑龙江 oooa,黑龙江 oooo,黑龙江 ooao,黑龙江 oaao,0",
+    "0,0,0,新疆 aaoa,0,0,0,0,0,0,0,0,0,0,0,内蒙古 oooa,内蒙古 oaoo,吉林 aaaa,黑龙江 ooaa,黑龙江 oaao,吉林 aooa,吉林 aaao,0",
+    "0,0,新疆 aooa,新疆 oooo,新疆 aaoo,0,0,0,0,0,0,0,内蒙古 aooa,内蒙古 aoao,内蒙古 aoao,内蒙古 ooao,内蒙古 ooao,内蒙古 aaao,吉林 aoaa,吉林 aooo,吉林 oaao,0,0",
+    "0,新疆 aooa,新疆 oooo,新疆 oooo,新疆 oooo,新疆 aooo,新疆 aaao,甘肃 aaoa,内蒙古 aoaa,内蒙古 aooo,内蒙古 aoao,内蒙古 aoao,内蒙古 oaao,陕西 aaaa,山西 aooa,山西 aaao,河北 aaaa,辽宁 aoaa,辽宁 aaoo,吉林 oaaa,0,0,0",
+    "新疆 aoaa,新疆 oooo,新疆 oooo,新疆 oooo,新疆 oooo,新疆 oaao,甘肃 aoaa,甘肃 ooao,甘肃 aaoo,内蒙古 oaaa,宁夏 aaoa,陕西 aoaa,陕西 aaoo,山西 aooa,山西 oaoo,河北 aaoa,北京 aaaa,河北 aaaa,辽宁 oaaa,0,0,0,0",
+    "0,新疆 ooaa,新疆 ooao,新疆 ooao,新疆 oaao,青海 aooa,青海 aooo,青海 aaoo,甘肃 ooaa,甘肃 aaoo,宁夏 oaaa,甘肃 aaoa,陕西 oaoa,山西 oooa,山西 oaoo,河北 ooaa,河北 aaoo,天津 aaaa,0,0,0,0,0",
+    "0,西藏 aooa,西藏 aooo,西藏 aaoo,青海 aooa,青海 oooo,青海 oooo,青海 oooo,青海 aaao,甘肃 oooa,甘肃 aooo,甘肃 oaao,陕西 oaoa,山西 ooaa,山西 oaao,河南 aaoa,河北 oaaa,山东 aooa,山东 aaao,0,0,0,0",
+    "0,西藏 ooaa,西藏 oooo,西藏 oaoo,青海 ooaa,青海 oooo,青海 ooao,青海 oaoo,甘肃 aoaa,甘肃 ooao,甘肃 oaao,陕西 aooa,陕西 oooo,陕西 aaoo,河南 aooa,河南 oaoo,山东 aoaa,山东 oaao,0,0,0,0,0",
+    "0,0,西藏 ooaa,西藏 oooo,西藏 aaoo,青海 oaaa,四川 aaaa,青海 oaaa,四川 aooa,四川 aaoo,陕西 aoaa,陕西 ooao,陕西 ooao,陕西 oaao,河南 ooaa,河南 oooo,河南 aaoo,安徽 aaoa,江苏 aaoa,0,0,0,0",
+    "0,0,0,西藏 ooaa,西藏 ooao,西藏 aooo,西藏 aaoo,四川 aooa,四川 oooo,四川 oooo,四川 aaao,重庆 aooa,重庆 aaao,湖北 aooa,湖北 aaoo,河南 ooaa,河南 oaao,安徽 oaoa,江苏 ooaa,江苏 aaao,0,0,0",
+    "0,0,0,0,0,西藏 ooaa,西藏 oaao,四川 ooaa,四川 oooo,四川 oaao,重庆 aoaa,重庆 oaoo,湖北 aoaa,湖北 ooao,湖北 ooao,湖北 aaao,安徽 aoaa,安徽 oaao,浙江 aaoa,上海 aaaa,0,0,0",
+    "0,0,0,0,0,0,云南 aoaa,云南 aaoo,四川 oaaa,云南 aaaa,贵州 aaoa,重庆 oaaa,湖南 aooa,湖南 aooo,湖南 aaao,江西 aooa,江西 aaoo,浙江 aoaa,浙江 ooao,浙江 aaao,0,0,0",
+    "0,0,0,0,0,0,0,云南 oooa,云南 aaoo,贵州 aooa,贵州 oooo,贵州 aaoo,湖南 ooaa,湖南 oaoo,江西 aoaa,江西 oooo,江西 oaao,福建 aooa,福建 aaao,0,0,0,0",
+    "0,0,0,0,0,0,云南 aoaa,云南 oooo,云南 oaoo,贵州 ooaa,贵州 ooao,贵州 oaao,广西 aaoa,湖南 ooaa,湖南 aaao,江西 oaaa,福建 aoaa,福建 oaao,0,0,0,0,0",
+    "0,0,0,0,0,0,0,云南 oooa,云南 ooao,云南 aaao,广西 aoaa,广西 aooo,广西 oooo,广西 aaao,广东 aooa,广东 aooo,广东 aaao,0,台湾 aaoa,0,0,0,0",
+    "0,0,0,0,0,0,0,云南 oaaa,0,0,0,广西 ooaa,广西 oaao,广东 aoaa,广东 ooao,广东 oaao,0,0,台湾 oaaa,0,0,0,0",
+    "0,0,0,0,0,0,0,0,0,0,0,0,广东 aaaa,澳门 aaaa,香港 aaaa,0,0,0,0,0,0,0,0",
+    "0,0,0,0,0,0,0,0,0,0,0,海南 aoaa,海南 aaao,0,0,0,0,0,0,0,0,0,0"
+];
+
+let dy="";
+
+zgdt.forEach(row=>{
+dy+="<tr>";
+row.split(",").forEach(cell=>{
+    ce=cell.split(" ")[0];
+    dy+=cell==="0" 
+        ? "<td class='dttd'>&nbsp;　&nbsp;</td>"
+        : `<td class="dttc ${cell}" data-tooltip="${ce}`+
+        ((dtts[ce])?`／${dtts[ce]}`:"")+
+        '"'+
+        ((dtdj[ce])?` onclick="点击('${ce}')"`:"")+
+        ((dtdk[ce])?" style='box-shadow:inset 100px 100px "+((dtdk[ce]==1)?"#00000000":dtdk[ce])+"'":"")+
+        `>&nbsp;　&nbsp;</td>`;
+});
+dy+="</tr>";
+});
+
+document.getElementById("dy_dt").innerHTML=dy;
+
+function 点击(o){
+window.location.href=dtdj[o];
+}
+
+
+const tooltip=document.getElementById('tooltip');
+const hoverItems=document.querySelectorAll('.dttc');
+
+hoverItems.forEach(item=>{
+    item.addEventListener('mouseenter',(e)=>{
+        let text=item.dataset.tooltip.split("／");
+        tooltip.innerHTML=`<small>${text[0]}</small>`+((text[1])?"<br>"+text[1]:"");
+        document.getElementById("dy_sty").innerHTML="."+text[0]+"{box-shadow:inset 100px 100px #74B8E580 !important;}";
+        tooltip.style.display='block';
+    });
+
+    item.addEventListener('mousemove',(e)=>{
+        const x=e.clientX+10;
+        const y=e.clientY+10;
+        tooltip.style.left=x+'px';
+        tooltip.style.top=y+'px';
+    });
+
+    item.addEventListener('mouseleave',()=>{
+        tooltip.style.display='none';
+        document.getElementById("dy_sty").innerHTML="";
+    });
+});
